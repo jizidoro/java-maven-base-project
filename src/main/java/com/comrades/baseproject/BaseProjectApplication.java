915884831942a17008a3b54ed36640c7014f7642
jhonatan antunes <jhonatan.izidoro@d1000varejofarma.com.br>
2021-07-29 20:15:02 -0300

    m2 funcionando

--- src/main/java/com/comrades/baseproject/BaseProjectApplication.java
+++ src/main/java/com/comrades/baseproject/BaseProjectApplication.java
@@ -7,30 +7,44 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class BaseProjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BaseProjectApplication.class, args);
+        fetchData();
     }
 
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
+    public static List<Employee> fetchData() {
+        final String SQL_QUERY = "select * from emp";
+        List<Employee> employees = null;
+        try (Connection con = DataSource.getConnection(); PreparedStatement pst = con.prepareStatement(SQL_QUERY); ResultSet rs = pst.executeQuery();) {
+            employees = new ArrayList<Employee>();
+            Employee employee;
+            while (rs.next()) {
+                employee = new Employee();
+                employee.setEmpNo(rs.getInt("empno"));
+                employee.setEname(rs.getString("ename"));
+                employee.setJob(rs.getString("job"));
+                employee.setMgr(rs.getInt("mgr"));
+                employee.setHiredate(rs.getDate("hiredate"));
+                employee.setSal(rs.getInt("sal"));
+                employee.setComm(rs.getInt("comm"));
+                employee.setDeptno(rs.getInt("deptno"));
+                employees.add(employee);
             }
-
-        };
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return employees;
     }
-
 }
 
 
